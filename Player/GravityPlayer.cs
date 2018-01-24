4c6f88800ebd68a625f4382af880a7e0e6decc78
TheWizBow <dave.van.staden@outlook.com>
2018-01-24 14:11:32 +0100

    walk/gravity

---
+++ Player/GravityPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPlayer : MonoBehaviour
+{
+
+    public bool isGrounded;
+    private void Update()
+    {
+        RaycastHit hit;
+        //checks if the player is on the floor or in the air with an raycast
+        if (Physics.Raycast(this.transform.position, -transform.up, out hit))
+        {
+            
+        }
+    }
+}
