c2f3141067eb17c7718c8be52ef74091b9cfbe1f
MeesMD <meesmilan@hotmail.nl>
2018-01-25 09:15:07 +0100

    Revert "Revert "Start""
    This reverts commit 53d789a5d97f9cb68c5744fee1081495e712213a.

---
+++ Assets/Scripts/SpeedBoostScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoostScript : MonoBehaviour
+{
+    public Camera camerMain;
+    public Camera camerSide;
+    bool boostTouch;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        boostTouch = GameObject.Find("Player").GetComponent<PlayerMovement>().boostTouch;
+
+        if (boostTouch == true)
+        {
+            camerMain.enabled = false;
+            camerSide.enabled = true;
+        }
+        else
+        {
+            camerMain.enabled = true;
+            camerSide.enabled = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/TrapUpDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapUpDown : MonoBehaviour
+{
+    private float height = 2f;
+    private float _maxSpeed = 2;
+
+    void Start()
+    {
+        //StartCoroutine()
+    }
+
+    void moveVertical()
+    {
+        float newY = Mathf.Sin(Time.time * _maxSpeed);
+        transform.position = new Vector3(transform.position.x, newY * height, transform.position.z);
+    }
+}
+    
